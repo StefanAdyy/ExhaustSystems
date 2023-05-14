da2bee2dc04ccab20bcdb166a81a78b2701d034e
StefanAdyy <stefanmorariu@rocketmail.com>
2023-05-14 15:32:58 +0300

    Added OrderPartService

---
+++ DataLayer/Entities/OrderPart.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Entities
+{
+    public class OrderPart:BaseEntity
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int PartId { get; set; }
+
+        [ForeignKey(nameof(OrderId))]
+        public Order Order { get; set; }
+
+        [ForeignKey(nameof(PartId))]
+        public Part Part { get; set; }
+    }
+}
