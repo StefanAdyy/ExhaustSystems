4ab80a7d518ad2fcb8818f56909e2b6e79ab2e78
StefanAdyy <stefanmorariu@rocketmail.com>
2023-05-14 15:12:29 +0300

    Update OrderRepository.cs

--- DataLayer/Repositories/OrderRepository.cs
+++ DataLayer/Repositories/OrderRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataLayer.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace DataLayer.Repositories
 {
-    public class OrderRepository
+    public class OrderRepository:RepositoryBase<Order>
     {
+        private readonly AppDbContext dbContext;
+
+        public OrderRepository(AppDbContext dbContext) : base(dbContext)
+        {
+            this.dbContext = dbContext;
+        }
     }
 }
