3cadb8c3b7682953d49db9aefcadd9962a1f5ab8
StefanAdyy <stefanmorariu@rocketmail.com>
2023-05-14 15:31:42 +0300

    Update PartRepository.cs

--- DataLayer/Repositories/PartRepository.cs
+++ DataLayer/Repositories/PartRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataLayer.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace DataLayer.Repositories
 {
-    internal class PartRepository
+    public class PartRepository:RepositoryBase<Part>
     {
+        private readonly AppDbContext dbContext;
+
+        public PartRepository(AppDbContext dbContext) : base(dbContext)
+        {
+            this.dbContext = dbContext;
+        }
     }
 }
