809575accb9cf0c696ef4d79fd9a922d90356458
StefanAdyy <stefanmorariu@rocketmail.com>
2023-05-14 15:31:35 +0300

    Updated OrderPartRepository

--- DataLayer/Repositories/OrderPartRepository.cs
+++ DataLayer/Repositories/OrderPartRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataLayer.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace DataLayer.Repositories
 {
-    internal class OrderPartRepository
+    public class OrderPartRepository:RepositoryBase<OrderPart>
     {
+        private readonly AppDbContext dbContext;
+
+        public OrderPartRepository(AppDbContext dbContext):base(dbContext)
+        {
+            this.dbContext = dbContext;
+        }
     }
 }
